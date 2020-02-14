eb4d74ed9fa458c4753ce6ba1d89724df75da709
Daniel Morales <daniel.a.moralesalt@gmail.com>
2020-02-14 11:24:46 -0500

    Created a Private Route to render the data

--- friends/src/App.js
+++ friends/src/App.js
@@ -2,6 +2,8 @@ import React from 'react';
 import './App.css';
 import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
 import Login from "./components/Login";
+import PrivateRoute from "./components/PrivateRoute";
+import Profile from "./components/Profile"
 
 function App() {
   return (
@@ -10,12 +12,13 @@ function App() {
         <header>
           <ul>
             <li><Link to="/login">Login</Link></li>
-            {/* <li><Link to="/">Home</Link></li> */}
+            <li><Link to="/profile">Home</Link></li>
           </ul>
         </header>
-
-        <Route path="/login" component={Login} />
-
+        <Switch>
+          <Route path="/login" component={Login} />
+          <PrivateRoute path="/profile" component={Profile} />
+        </Switch>
       </div>
     </Router>
   );
