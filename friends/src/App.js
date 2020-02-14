19e86eb3d1bfc5dfa3d1c3b3abd31705370b6f7c
Daniel Morales <daniel.a.moralesalt@gmail.com>
2020-02-14 10:13:14 -0500

    Built a Login form and set its route

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,12 +1,23 @@
 import React from 'react';
 import './App.css';
+import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
 import Login from "./components/Login";
 
 function App() {
   return (
-    <div className="App">
-      <Login />
-    </div>
+    <Router>
+      <div className="App">
+        <header>
+          <ul>
+            <li><Link to="/login">Login</Link></li>
+            {/* <li><Link to="/">Home</Link></li> */}
+          </ul>
+        </header>
+
+        <Route path="/login" component={Login} />
+
+      </div>
+    </Router>
   );
 }
 
--- friends/src/components/Login.js
+++ friends/src/components/Login.js
@@ -1,15 +1,42 @@
 import React, { useState } from "react";
+import axios from "axios";
 
 const Login = () => {
-  const [credentials, setCredentials] = useState({ useranme: "", password: "" })
+  const [credentials, setCredentials] = useState({ username: "", password: "" })
+  const [isLoading, setIsLoading] = useState(false)
+
+  const handleChange = e => {
+    setCredentials({ ...credentials, [e.target.name]: e.target.value })
+  }
+
+  const handleLogin = e => {
+    e.preventDefault();
+    setIsLoading(true)
+    axios
+      .post("http://localhost:5000/api/login", credentials)
+      .then(res => {
+        localStorage.setItem("token", res.data.payload);
+        setIsLoading(false);
+        console.log("Axios POST", res)
+      })
+      .catch(error => {
+        localStorage.removeItem("token");
+        setIsLoading(false)
+        console.log("Invalid login", error)
+      })
+    console.log("Form Submitted")
+    setCredentials({ username: "", password: "" })
+  }
 
   return (
     <>
-      <form>
-        <input type="text" name="username" value={credentials.useranme} />
-        <input type="password" name="password" value={credentials.password} />
-        <button type="submit">Submit</button>
-      </form>
+      {isLoading ? (<h2>Loading...</h2>) : (
+        <form onSubmit={handleLogin}>
+          <input type="text" name="username" value={credentials.username} onChange={handleChange} required />
+          <input type="password" name="password" value={credentials.password} onChange={handleChange} required />
+          <button type="submit">Submit</button>
+        </form>
+      )}
     </>
   )
 }
